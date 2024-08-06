5bcc3f87edfd25dd9a2d26ff61706b544a9c0cb2
MFaad1 <faadsardar123@gmail.com>
2024-08-06 14:18:37 +0500

    changes added for redux

---
+++ client/src/redux/slices/Admin/AdminsSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: {
+  userLoading: boolean,
+  loggedInUser: any | undefined,
+  assistantResponse: string;
+} = {
+  userLoading: false,
+  loggedInUser: undefined,
+  assistantResponse: '',
+};
+
+const usersSlice = createSlice({
+  name: 'usersSlice',
+  initialState: initialState,
+  reducers: {
+    loggedInAdminAction: (state, action) => {
+      state.loggedInUser = action.payload;
+    },
+  },
+
+});
+
+export const { loggedInAdminAction} = usersSlice.actions;
+export default usersSlice.reducer;
