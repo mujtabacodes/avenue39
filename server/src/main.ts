ca25acdd334ddaeb539dabfa9818965eaf3a63d0
MFaad1 <faadsardar123@gmail.com>
2024-08-08 14:36:37 +0500

    fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3300);
 }
