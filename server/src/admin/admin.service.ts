8b89a7f21c3aa809173c30861583219c11bf1c65
MFaad1 <faadsardar123@gmail.com>
2024-08-10 15:23:25 +0500

    fixes

--- server/src/admin/admin.service.ts
+++ server/src/admin/admin.service.ts
@@ -6,7 +6,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { LoginDto } from 'src/users/dto/user.dto';
 import { hashPassword, verifyPassword } from 'src/utils/func';
 import * as jwt from 'jsonwebtoken';
--- server/src/reviews/reviews.service.ts
+++ server/src/reviews/reviews.service.ts
@@ -1,5 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { customHttpException } from 'src/utils/helper';
 import { AddReviewDto } from './dto/reviews.dto';
 
