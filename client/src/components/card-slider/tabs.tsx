6ac6651e3136f6dc36a6229b5510718e2aa39bf3
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-08-12 05:16:03 -0700

    Fixed the policy issue and dashboard issue

--- client/src/components/card-slider/tabs.tsx
+++ client/src/components/card-slider/tabs.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import SliderComponent from './card-slider'; // Make sure to use the correct path
+import SliderComponent from './card-slider'; 
 import { ISliderData } from '@/types/types';
 import { useSelector } from 'react-redux';
 import { State } from '@/redux/store';
