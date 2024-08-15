dbdb21ac39106fa0113ca83a4529fae471b2147a
MFaad1 <faadsardar123@gmail.com>
2024-08-15 15:35:20 +0500

    fixes

--- client/src/app/AR/[imageurl]/page.tsx
+++ client/src/app/AR/[imageurl]/page.tsx
@@ -52,7 +52,6 @@ console.log(TryAtHomeImage?.imageUrl, " TryAtHomeImage")
 {
 !TryAtHomeImage ? 'Unexpected error':
 <>
-<div>My Post: {TryAtHomeImage.imageUrl}</div>
 <ARExperience ImageUrl={TryAtHomeImage.imageUrl}/>
 </>
 
