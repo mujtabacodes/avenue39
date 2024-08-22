90e7d736afee11439bb18aa81fa6e6f9ff622384
wajid-fareed <wajidfareed103@gmail.com>
2024-08-22 12:14:02 +0500

    zoom issue fixed

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['furniturezone.pk', 'example.com', 'res.cloudinary.com'],
+        domains: ['furniturezone.pk', 'example.com', 'res.cloudinary.com', 'unsplash.com'],
     },
 };
 
