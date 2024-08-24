b2a04e8d6b75b83b8862afab66a13ff5a981f658
MFaad1 <faadsardar123@gmail.com>
2024-08-24 12:16:55 +0500

    fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,19 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
- app.enableCors();
+ app.enableCors({
+    origin: [
+      'https://*.vercel.app',
+      'http://localhost:3000',
+      'https://avenue39.vercel.app',
+      'https://avenue39-git-dev-interior-films-projects.vercel.app',
+      "https://avenue39-73ck3f2xg-interior-films-projects.vercel.app",
+      'https://avenue39-git-faad-dev-interior-films-projects.vercel.app',
+      "https://avenue39-git-mujtaba-dev-interior-films-projects.vercel.app"
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
