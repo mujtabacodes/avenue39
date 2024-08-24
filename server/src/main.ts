292bbb79fa055e3e18fb7def17192c58b2ee99ca
MFaad1 <faadsardar123@gmail.com>
2024-08-24 16:42:05 +0500

    fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,16 +8,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
- app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://*vercel.app',
-      'https://avenue39.vercel.app',
-    ],
-  });
+ app.enableCors();
 
   app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe());
+  
   app.use(cookieParser());
   const config = new DocumentBuilder()
     .setTitle('Avenue39')
