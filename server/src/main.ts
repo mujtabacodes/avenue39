6c47680de887817bb8fdd432493c2260d1a2e748
MFaad1 <faadsardar123@gmail.com>
2024-10-02 19:16:28 +0500

    fixes

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://avenue39.vercel.app'],
+    origin: ['http://localhost:3000','http://localhost:3001', 'https://avenue39.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
   });
