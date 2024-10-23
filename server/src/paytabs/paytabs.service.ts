de8f9bdb6fb3cb1c8adc53010cf68cbee372c41f
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-10-23 14:59:38 +0500

    fixes: Update Call back too

--- server/src/paytabs/paytabs.service.ts
+++ server/src/paytabs/paytabs.service.ts
@@ -24,7 +24,7 @@ export class PaytabsService {
       cart_description: `Order for ${firstName} ${lastName}`,
       cart_currency: 'AED',
       cart_amount: amount,
-      callback: 'https://avenue39.vercel.app',
+      callback: 'https://avenue39.vercel.app/thanks',
       return: 'https://avenue39.vercel.app/thanks',
       customer_details: {
         name: `${firstName} ${lastName}`,
