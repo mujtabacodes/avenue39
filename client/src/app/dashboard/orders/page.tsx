2ef07ca1e227fb3d4ce6843552a11551de50542c
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2024-11-13 17:56:29 +0500

    filter by object in order page

--- client/src/app/dashboard/orders/page.tsx
+++ client/src/app/dashboard/orders/page.tsx
@@ -2,18 +2,24 @@
 import Breadcrumb from '@/components/Dashboard/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '@/components/Dashboard/Layouts/DefaultLayout';
 import React, { useEffect, useState } from 'react';
-import FilterTable from '@/components/Dashboard/Tables/FilterTable';
-import { ordercolumns, Orderdata } from '@/data/table';
 import { Table } from 'antd';
+import { ordercolumns, Orderdata } from '@/data/table';
+
 
 interface Order {
-  id: string;
+  OrderId: string;  
   Name?: string;
-  [key: string]: any; 
+  Address?: string;
+  Country?: string;
+  Email?: string;
+  PhoneNumber?: string;
+  [key: string]: any;  
 }
+
 const Orders = () => {
-  const [orderData, setOrderData] = useState<any[]>([]);
+  const [orderData, setOrderData] = useState<Order[]>([]);  
   const [searchTerm, setSearchTerm] = useState<string>('');
+
   useEffect(() => {
     setOrderData(Orderdata);
   }, []);
@@ -21,21 +27,26 @@ const Orders = () => {
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchTerm(value);
+
     setOrderData(
-      Orderdata.filter((order) =>
-        order.Name
-          ? order.Name.toLowerCase().includes(value.toLowerCase())
-          : false
-      )
+      Orderdata.filter((order) => {
+        const lowercasedSearchTerm = value.toLowerCase();
+        return (
+          (order.Name && order.Name.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Address && order.Address.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Country && order.Country.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Email && order.Email.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.PhoneNumber && order.PhoneNumber.includes(value)) ||
+          (order.OrderId && order.OrderId.includes(value))
+        );
+      })
     );
   };
-  
 
   return (
     <DefaultLayout>
       <Breadcrumb pageName={'View Orders'} />
-      {/* <FilterTable data={Orderdata} columns={ordercolumns} /> */}
-      <div className="flex justify-between mb-4 items-center text-dark dark:text-white ">
+      <div className="flex justify-between mb-4 items-center text-dark dark:text-white">
         <input
           className="peer lg:p-3 p-2 block outline-none border rounded-md border-gray-200 dark:bg-boxdark dark:bg-transparent dark:border-white text-11 xs:text-sm dark:focus:border-primary focus:border-dark focus:ring-dark-500 disabled:opacity-50 disabled:pointer-events-none dark:text-black"
           type="search"
@@ -44,13 +55,14 @@ const Orders = () => {
           onChange={handleSearchChange}
         />
       </div>
-      {orderData && orderData.length > 0 ? (
+
+      {orderData.length > 0 ? (
         <Table
           className="overflow-x-scroll lg:overflow-auto"
           dataSource={orderData}
           columns={ordercolumns}
           pagination={false}
-          rowKey="id"
+          rowKey="OrderId" 
         />
       ) : (
         'No Orders found'
