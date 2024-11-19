92d257b7eff31a4d93ed9ace00cc3863cbbfb1c5
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-11-19 16:01:14 +0500

    fixes: Order history (API and api integeration)

--- client/src/app/dashboard/abundant/page.tsx
+++ client/src/app/dashboard/abundant/page.tsx
@@ -1,27 +1,103 @@
-
-'use client'
+'use client';
 import Breadcrumb from '@/components/Dashboard/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '@/components/Dashboard/Layouts/DefaultLayout';
 import React, { useEffect, useState } from 'react';
 import FilterTable from '@/components/Dashboard/Tables/FilterTable';
 import { ordercolumns, Orderdata } from '@/data/table';
-import { Table } from 'antd';
-
-
-const Abundant  = () => {
+import { Modal, Table } from 'antd';
+import axios from 'axios';
+import { formatDate } from '@/config';
+import { LuView } from 'react-icons/lu';
+import Image from 'next/image';
+const Abundant = () => {
   const [abundantOrderData, setAbundantOrderData] = useState<any[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [visible, setVisible] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState<any[]>([]);
   useEffect(() => {
-    setAbundantOrderData(Orderdata);
+    async function getOrderHistory() {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/sales-record/order_history`,
+      );
+      console.log(res.data);
+      const data = res.data.filter(
+        (order: any) => order.paymentStatus.paymentStatus === false,
+      );
+      console.log('----------- AYLO ---------');
+      console.log(data);
+      setAbundantOrderData(data);
+    }
+    getOrderHistory();
   }, []);
+
+  const ordercolumns = [
+    {
+      title: 'OrderId',
+      dataIndex: 'orderId',
+      key: 'orderId',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'user_email',
+      key: 'user_email',
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+
+      render: (createdAt: string) => formatDate(createdAt),
+    },
+    {
+      title: 'View Products',
+      dataIndex: 'view',
+      key: 'view',
+      render: (text: any, record: any) => (
+        <span className="flex justify-center">
+          <LuView
+            onClick={() => handleViewProducts(record.products)}
+            className="cursor-pointer text-green-500"
+            size={30}
+          />
+        </span>
+      ),
+    },
+  ];
+
+  const handleViewProducts = (products: any[]) => {
+    setSelectedProducts(products);
+    console.log('================= CHAL OYE ================');
+    console.log(products);
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchTerm(value);
     setAbundantOrderData(
       Orderdata.filter((order) =>
-        order.Name ? order.Name.toLowerCase().includes(value.toLowerCase()) : false
-      )
+        order.Name
+          ? order.Name.toLowerCase().includes(value.toLowerCase())
+          : false,
+      ),
     );
   };
   return (
@@ -38,13 +114,42 @@ const Abundant  = () => {
         />
       </div>
       {abundantOrderData ? (
-        <Table
-          className="overflow-x-scroll lg:overflow-auto"
-          dataSource={abundantOrderData}
-          columns={ordercolumns}
-          pagination={false}
-          rowKey="id"
-        />
+        <>
+          <Table
+            className="overflow-x-scroll lg:overflow-auto"
+            dataSource={abundantOrderData}
+            columns={ordercolumns}
+            pagination={false}
+            rowKey="id"
+          />
+          <Modal
+            title="Order Detail"
+            visible={visible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={null}
+          >
+            {selectedProducts.map((product) => (
+              <div className="flex gap-2 items-center mt-2" key={product.id}>
+                <Image
+                  className="rounded-md"
+                  width={100}
+                  height={100}
+                  src={product.productData.posterImageUrl}
+                  alt={product.productData.name}
+                />
+                <div>
+                  <h3>{product.productData.name}</h3>
+                  <p>
+                    Price: {product.productData.price}{' '}
+                    {product.productData.currency}
+                  </p>
+                  <p>Quantity: {product.quantity}</p>
+                </div>
+              </div>
+            ))}
+          </Modal>
+        </>
       ) : (
         'No Orders found'
       )}
--- client/src/app/dashboard/orders/page.tsx
+++ client/src/app/dashboard/orders/page.tsx
@@ -1,28 +1,97 @@
-'use client'
+'use client';
 import Breadcrumb from '@/components/Dashboard/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '@/components/Dashboard/Layouts/DefaultLayout';
 import React, { useEffect, useState } from 'react';
-import { Table } from 'antd';
+import { Modal, Table } from 'antd';
 import { ordercolumns, Orderdata } from '@/data/table';
-
+import axios from 'axios';
+import Image from 'next/image';
+import { LuView } from 'react-icons/lu';
+import { formatDate } from '@/config';
 
 interface Order {
-  OrderId: string;  
+  OrderId: string;
   Name?: string;
   Address?: string;
   Country?: string;
   Email?: string;
   PhoneNumber?: string;
-  [key: string]: any;  
+  [key: string]: any;
 }
 
 const Orders = () => {
-  const [orderData, setOrderData] = useState<Order[]>([]);  
+  const [orderData, setOrderData] = useState<Order[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [visible, setVisible] = useState(false);
+  const [selectedProducts, setSelectedProducts] = useState<any[]>([]);
 
   useEffect(() => {
-    setOrderData(Orderdata);
+    async function getOrderHistory() {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/sales-record/order_history`,
+      );
+      const data = res.data.filter(
+        (order: any) => order.paymentStatus.paymentStatus === true,
+      );
+      setOrderData(data);
+    }
+    getOrderHistory();
   }, []);
+  const ordercolumns = [
+    {
+      title: 'OrderId',
+      dataIndex: 'orderId',
+      key: 'orderId',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'user_email',
+      key: 'user_email',
+    },
+    {
+      title: 'Phone Number',
+      dataIndex: 'phoneNumber',
+      key: 'phoneNumber',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+
+      render: (createdAt: string) => formatDate(createdAt),
+    },
+    {
+      title: 'View Products',
+      dataIndex: 'view',
+      key: 'view',
+      render: (text: any, record: any) => (
+        <span className="flex justify-center">
+          <LuView
+            onClick={() => handleViewProducts(record.products)}
+            className="cursor-pointer text-green-500"
+            size={30}
+          />
+        </span>
+      ),
+    },
+  ];
+  const handleViewProducts = (products: any[]) => {
+    setSelectedProducts(products);
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -32,14 +101,18 @@ const Orders = () => {
       Orderdata.filter((order) => {
         const lowercasedSearchTerm = value.toLowerCase();
         return (
-          (order.Name && order.Name.toLowerCase().includes(lowercasedSearchTerm)) ||
-          (order.Address && order.Address.toLowerCase().includes(lowercasedSearchTerm)) ||
-          (order.Country && order.Country.toLowerCase().includes(lowercasedSearchTerm)) ||
-          (order.Email && order.Email.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Name &&
+            order.Name.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Address &&
+            order.Address.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Country &&
+            order.Country.toLowerCase().includes(lowercasedSearchTerm)) ||
+          (order.Email &&
+            order.Email.toLowerCase().includes(lowercasedSearchTerm)) ||
           (order.PhoneNumber && order.PhoneNumber.includes(value)) ||
           (order.OrderId && order.OrderId.includes(value))
         );
-      })
+      }),
     );
   };
 
@@ -57,13 +130,42 @@ const Orders = () => {
       </div>
 
       {orderData.length > 0 ? (
-        <Table
-          className="overflow-x-scroll lg:overflow-auto"
-          dataSource={orderData}
-          columns={ordercolumns}
-          pagination={false}
-          rowKey="OrderId" 
-        />
+        <>
+          <Table
+            className="overflow-x-scroll lg:overflow-auto"
+            dataSource={orderData}
+            columns={ordercolumns}
+            pagination={false}
+            rowKey="OrderId"
+          />
+          <Modal
+            title="Order Detail"
+            visible={visible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            footer={null}
+          >
+            {selectedProducts.map((product) => (
+              <div className="flex gap-2 items-center mt-2" key={product.id}>
+                <Image
+                  className="rounded-md"
+                  width={100}
+                  height={100}
+                  src={product.productData.posterImageUrl}
+                  alt={product.productData.name}
+                />
+                <div>
+                  <h3>{product.productData.name}</h3>
+                  <p>
+                    Price: {product.productData.price}{' '}
+                    {product.productData.currency}
+                  </p>
+                  <p>Quantity: {product.quantity}</p>
+                </div>
+              </div>
+            ))}
+          </Modal>
+        </>
       ) : (
         'No Orders found'
       )}
