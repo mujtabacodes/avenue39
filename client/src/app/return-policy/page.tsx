66759bab64b741c29678c3b2d44fa84b5235b9a6
MFaad1 <faadsardar123@gmail.com>
2025-01-08 10:20:17 +0500

    removed extrac div

--- client/src/app/return-policy/page.tsx
+++ client/src/app/return-policy/page.tsx
@@ -32,7 +32,7 @@ const ReturnPolicyPage: React.FC = () => {
   return (
     <>
       <TopHero title="Return Policy" breadcrumbs={TReturnPolicybredcrumbs} />
-      <div>
+   
         <div className="max-w-5xl mx-auto p-4">
           {returnPolicyData.map((section: any, index: number) => (
             <div key={index} className="mb-6">
@@ -67,7 +67,6 @@ const ReturnPolicyPage: React.FC = () => {
             </div>
           ))}
         </div>
-      </div>
     </>
   );
 };
