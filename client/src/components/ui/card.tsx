0bedb5622970740713fcf1f81c11dbdbf1f8c0fc
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-10 12:20:50 +0500

    code fixexd

--- client/src/components/ui/card.tsx
+++ client/src/components/ui/card.tsx
@@ -95,7 +95,7 @@ const Card: React.FC<CardProps> = ({
     return <CardSkeleton skeletonHeight={skeletonHeight} />;
   }
   return (
-    <div className={`text-center relative product-card group hover:cursor-pointer mb-2  ${slider ? '':'flex flex-col justify-between'} h-full  p-1 rounded-[35px]`}>
+    <div className={`text-center relative product-card  hover:cursor-pointer mb-2  ${slider ? '':'flex flex-col justify-between'} h-full  p-1 rounded-[35px]`}>
       <div className="relative w-full overflow-hidden rounded-[35px]">
         {loading ? (
           <CardSkeleton skeletonHeight={skeletonHeight} />
@@ -207,18 +207,33 @@ const Card: React.FC<CardProps> = ({
           onClick={(e) => handleEventProbation(e)}
         >
           <button
-            className={` my-1 w-full h-8 text-primary border border-primary rounded-full flex items-center justify-center whitespace-nowrap gap-2 hover:bg-primary hover:text-white ${slider ? "px-6":"px-2"}`}
+            className={` my-1 w-full h-8 text-primary border border-primary group rounded-full flex items-center justify-center whitespace-nowrap gap-2 hover:bg-primary hover:text-white ${slider ? "px-6":"px-2"}`}
             onClick={(e) => handleAddToCard(e)}
           >
-            <GiShoppingCart className='-scale-x-100'/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="14.481" height="14.536" viewBox="0 0 14.481 14.536" className='group-hover:fill-white'>
+              <path id="Path_424" data-name="Path 424" d="M11.8,1.49H.768c-.722,0-.683.292-.62,1.037L.558,7.76c.07.834.011.632.894.758L9.57,9.551,9.14,10.8H.4c-.118.442-.279,1.163-.4,1.656H1.453l.14-.5H8.578c1.6-.027,1.442.407,1.826-.978L13.159.959h1.322V0h-2.32c-.108.4-.257,1.082-.357,1.49ZM8.13,12.293a1.121,1.121,0,1,0,1.121,1.121A1.122,1.122,0,0,0,8.13,12.293Zm-4.625,0a1.121,1.121,0,1,0,1.121,1.121A1.122,1.122,0,0,0,3.5,12.293Zm7.333-7.2H9.052L9.7,2.385h1.884l-.218.811h-.007l-.522,1.9ZM8.766,2.386,8.118,5.095H6.4l.651-2.706,1.718,0Zm-2.653,0L5.463,5.095H3.817l.648-2.7,1.648,0Zm-2.583,0L2.882,5.1H1.235L1.053,2.924C1,2.319.9,2.4,1.482,2.4l2.048,0ZM1.293,5.783H2.717l-.47,1.959-.116-.015c-.718-.1-.671.062-.727-.616L1.293,5.783Zm1.86,2.083.5-2.083H5.3l-.552,2.3L3.152,7.866Zm2.5.339.583-2.424H7.954L7.319,8.433,5.65,8.206Zm2.574.351.664-2.774h1.761L9.825,8.776l-1.6-.219Z" fill-rule="evenodd"/>
+            </svg>
             <span className="text-12 font-medium">Add to Cart</span>
           </button>
+
           <Dialog>
             <DialogTrigger className='w-full'>
             <button
-            className={`my-1 w-full h-8 whitespace-nowrap text-secondary border border-primary bg-primary rounded-full flex items-center justify-center gap-2 hover:bg-secondary hover:text-primary ${slider ? "px-6":"px-2"}`} 
+            className={`my-1 w-full h-8 whitespace-nowrap text-secondary border border-primary group bg-primary rounded-full flex items-center justify-center gap-2 hover:bg-secondary hover:text-primary ${slider ? "px-6":"px-2"}`} 
           >
-            <LuEye />
+            <svg xmlns="http://www.w3.org/2000/svg" width="17.41" height="9.475" viewBox="0 0 17.41 9.475" className='fill-white group-hover:fill-black group-hover:text-black'>
+              <g id="eye-svgrepo-com_1_" data-name="eye-svgrepo-com (1)" transform="translate(0 -100.736)">
+                <g id="Group_1742" data-name="Group 1742" transform="translate(0 100.736)">
+                  <path id="Path_428" data-name="Path 428" d="M8.705,110.211A10.685,10.685,0,0,1,2.612,108,15.425,15.425,0,0,1,.12,105.8a.492.492,0,0,1,0-.645,15.426,15.426,0,0,1,2.492-2.2,10.686,10.686,0,0,1,6.093-2.214A10.686,10.686,0,0,1,14.8,102.95a15.427,15.427,0,0,1,2.492,2.2.492.492,0,0,1,0,.645A15.428,15.428,0,0,1,14.8,108,10.685,10.685,0,0,1,8.705,110.211Zm-7.538-4.737A15.54,15.54,0,0,0,3.2,107.209a9.9,9.9,0,0,0,5.5,2.017,9.9,9.9,0,0,0,5.5-2.017,15.54,15.54,0,0,0,2.036-1.736,15.535,15.535,0,0,0-2.036-1.736,9.9,9.9,0,0,0-5.5-2.017,9.9,9.9,0,0,0-5.5,2.017A15.533,15.533,0,0,0,1.167,105.474Z" transform="translate(0 -100.736)" />
+                </g>
+                <g id="Group_1743" data-name="Group 1743" transform="translate(5.653 102.421)">
+                  <path id="Path_429" data-name="Path 429" d="M146.572,149.626a3.052,3.052,0,1,1,2.011-5.349.492.492,0,0,1-.649.741,2.068,2.068,0,1,0,.706,1.556.492.492,0,1,1,.985,0A3.056,3.056,0,0,1,146.572,149.626Z" transform="translate(-143.52 -143.521)" />
+                </g>
+                <g id="Group_1744" data-name="Group 1744" transform="translate(7.72 104.489)">
+                  <path id="Path_430" data-name="Path 430" d="M197,197.99a.985.985,0,1,1,.985-.985A.986.986,0,0,1,197,197.99Z" transform="translate(-196.02 -196.021)" />
+                </g>
+              </g>
+            </svg>
             <span className="text-12 font-medium">Quick View</span>
           </button>
            
