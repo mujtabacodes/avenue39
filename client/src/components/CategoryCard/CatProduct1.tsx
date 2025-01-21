a0e4e328aee15bfffb165508e83e6b57d11ef229
Wajid-Fareed <wajidfareed103@gmail.com>
2025-01-21 17:19:17 +0500

    home page ui fixes

--- client/src/components/CategoryCard/CatProduct1.tsx
+++ client/src/components/CategoryCard/CatProduct1.tsx
@@ -14,13 +14,13 @@ interface ICatProduct {
 const CatProduct1 = ({ reverse, CategoryName,products }: ICatProduct) => {
   return (
     <Container className="my-10">
-    <div className='relative p-2 md:p-4 border-2 border-[#707070] rounded-[87px]'>
+    <div className='relative px-2 md:px-6 border-2 border-[#707070] rounded-[87px]'>
     <div className="absolute -top-7 xs:-top-5 left-1/2 transform -translate-x-1/2 rounded-xl border bg-white xs:left-20 xs:transform-none">
         <p className="px-2 md:text-3xl font-Helveticalight capitalize text-center">
             {CategoryName}
         </p>
         </div>    
-      <div className={`my-10 `}>
+      <div className={`mt-12 `}>
   
         <div className={`grid grid-cols-12 sm:gap-8 ${reverse ? "hidden" : " block"}`}>
         <div className={`col-span-12 md:col-span-6 xl:col-span-7 `}>
@@ -35,7 +35,7 @@ const CatProduct1 = ({ reverse, CategoryName,products }: ICatProduct) => {
           {products.length < 1 && (
              <ProductSkeleton imageHeight="h-[210px] xl:h-[496.5px]" />
             )}
-              <ProductGrid products={products.slice(5, 6)} CardComponent={Card} slider={true} imageHeight="h-[210px] xl:h-[496.5px] w-full" />
+              <ProductGrid products={products.slice(5, 6)} CardComponent={Card} slider={true} imageHeight="h-[210px] xl:h-[496.5px] w-full" portSpace={'px-10'} />
           </div>
         </div>
         <div className={`grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:gap-8`}>
