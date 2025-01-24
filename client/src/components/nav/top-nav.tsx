d380dfc356b24f964c2b6e457c0b5a07a5745ee4
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-24 15:56:01 +0500

    ui fixed

--- client/src/components/nav/top-nav.tsx
+++ client/src/components/nav/top-nav.tsx
@@ -12,8 +12,8 @@ const TopNav = () => {
           <div className="w-fit">
           </div>
         </div>
-        <div className="w-full lg:max-w-[58%] xl:max-w-[43%] 2xl:max-w-[40%] ">
-        <p className='font-medium text-12 2xl:text-13 pl-2 2xl:pl-1 xl:whitespace-nowrap'>FALL CLEARANCE. SAVE UP TO 60% ON OUTDOOR, LIVING, DINING, BEDROOM & MORE. <Link className='text-red-600 border-b border-red-600' href={'products'} >SHOP SALE</Link></p>
+        <div className="w-full lg:max-w-[58%] xl:max-w-[43%] 2xl:max-w-[40%] 2xl:mr-6 ">
+        <p className='font-medium text-12 2xl:text-13 xl:whitespace-nowrap'>FALL CLEARANCE. SAVE UP TO 60% ON OUTDOOR, LIVING, DINING, BEDROOM & MORE. <Link className='text-red-600 border-b border-red-600' href={'products'} >SHOP SALE</Link></p>
         </div>
         <div className="gap-3 lg:gap-3 flex justify-end items-center w-2/12">
           <SocialLink socialSize="text-[30px]" />
