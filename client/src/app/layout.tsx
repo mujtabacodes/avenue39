0ac7bf1f04e971dbb43fdeebffe20c3d7d68d114
MFaad1 <faadsardar123@gmail.com>
2025-01-25 17:45:18 +0500

    fixes

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -10,6 +10,19 @@ import { Helvetica } from '@/components/language';
 export const metadata: Metadata = {
   title: 'Avenue 39',
   description: 'Best ecommerce website',
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
