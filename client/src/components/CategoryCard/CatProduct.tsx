e874a395b7427d0c4c4889a35af93df679a4152c
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-25 09:51:35 +0500

    link active

--- client/src/components/CategoryCard/CatProduct.tsx
+++ client/src/components/CategoryCard/CatProduct.tsx
@@ -5,6 +5,7 @@ import Card from '../ui/card';
 import ProductGrid from './ProductGrid';
 import { IProduct } from '@/types/types';
 import ProductSkeleton from '../Skaleton/productSkeleton';
+import Link from 'next/link';
 interface ICatProduct {
   reverse?: boolean;
   CategoryName: string;
@@ -13,18 +14,19 @@ interface ICatProduct {
   portSpace?: string;
   sofaHeight?: string;
   sideTableHeight?: string;
+  redirect?:string;
 }
-const CatProduct = ({ reverse, CategoryName, products , landHeight ,portSpace , sofaHeight ,sideTableHeight }: ICatProduct) => {
+const CatProduct = ({ reverse, CategoryName, products , landHeight ,portSpace , sofaHeight ,sideTableHeight,redirect }: ICatProduct) => {
   console.log("Products array: ", products);
 
   return (
     <Container className="my-10">
       <div className='relative px-2 md:px-6 border-2 border-[#707070] rounded-[40px] sm:rounded-[87px]'>
-        <div className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-xl border bg-white xs:left-20 xs:transform-none">
+        <Link href={`/products/${redirect}`} className="absolute -top-5 left-1/2 transform -translate-x-1/2 rounded-xl border bg-white xs:left-20 xs:transform-none hover:bg-black hover:text-white hover:border-black">
           <p className="px-2 md:text-3xl font-Helveticalight capitalize text-center">
             {CategoryName}
           </p>
-        </div>
+        </Link>
         <div className={`mt-6 sm:mt-12 flex ${reverse ? 'flex-col-reverse' : 'flex-col'}`}>
           <div className={`grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:gap-8`}>
             {products.length < 1 && Array(5).fill(null).map((_, index) => (
