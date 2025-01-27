feca0895dc39947675cd818bf773faefd802d4c6
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-27 16:54:25 +0500

    ramadan fixed

--- client/src/components/nav/top-nav.tsx
+++ client/src/components/nav/top-nav.tsx
@@ -4,6 +4,7 @@ import SocialLink from '../social-link';
 import Link from 'next/link';
 import Image from 'next/image';
 import ramadan1 from '../../../public/images/ramadan12.png';
+import ramadan2 from '../../../public/images/ramadan2.png';
 
 const TopNav = () => {
   return (
@@ -13,9 +14,9 @@ const TopNav = () => {
           <div className="w-fit"></div>
         </div>
         <div className="w-full lg:max-w-[58%] xl:max-w-[43%] 2xl:max-w-[40%] max-sm:pl-1 2xl:mr-6 relative">
-          <p className="flex justify-center gap-2 items-center font-medium text-12 2xl:text-13 xl:whitespace-nowrap">   
-          <Image className='absolute z-40 -left-32 -top-8' src={ramadan1} alt='ramadan1'/>
-            <p className='uppercase tracking-widest '>
+          <p className=" gap-2  font-medium text-12 2xl:text-13 xl:whitespace-nowrap">   
+          <Image className='absolute z-40 -left-40 -top-8 hidden xl:block' src={ramadan2} alt='ramadan1'/>
+            <p className='uppercase tracking-[1.6px] '>
               This Ramadan elevate your home’s style with the best offers.{' '}
               <Link
                 className="text-red-600 border-b border-red-600 hover:text-red-800"
@@ -24,7 +25,7 @@ const TopNav = () => {
                 SHOP SALE
               </Link>
             </p>        
-            <Image className='absolute z-40 -right-28 -top-8' src={ramadan1} alt='ramadan1'/>
+            <Image className='absolute z-40 -right-40 2xl:-right-28 -top-10 hidden xl:block' src={ramadan1} alt='ramadan1'/>
           </p>
         </div>
         <div className="gap-3 lg:gap-3 flex justify-end items-center w-2/12">
