1e5302ba658eb9f7d7be5126f338b47a26942145
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-27 17:08:45 +0500

    ramadan fixed

--- client/src/components/testimonial/custom-testimonal.tsx
+++ client/src/components/testimonial/custom-testimonal.tsx
@@ -9,7 +9,7 @@ interface TestimonialProps {
 const CustomTestimonal: React.FC<TestimonialProps> = ({testimonialitems}) => {
   return (
     <Container className='text-center py-14'>
-        <h2 className='font-medium text-[48px]'>Testimonial</h2>
+        <h2 className='font-medium text-[48px]'>Reviews</h2>
         <div className='grid grid-cols-1 xs:grid-cols-2 xl:flex xl:justify-between py-12 gap-6 '>
         {testimonialitems.map((card) => (
             <div key={card.id}>
