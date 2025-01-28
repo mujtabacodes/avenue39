359570047521941cf18d5caf80d3ca8fa108e5cf
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-28 11:22:03 +0500

    thumbnail

--- client/src/types/custom.d.ts
+++ client/src/types/custom.d.ts
@@ -1,7 +1,9 @@
 import React from "react";
-
+/* eslint-disable */
 declare namespace JSX {
     interface IntrinsicElements {
       ambientLight: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
     }
   }
+
+  /* eslint-enable */
