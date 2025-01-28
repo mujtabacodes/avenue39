8c1d513499b52410045537bd6e23a3dd862d0cf8
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-01-28 11:25:06 +0500

    main pull

--- client/src/types/custom.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-/* eslint-disable */
-declare namespace JSX {
-    interface IntrinsicElements {
-      ambientLight: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
-  }
-
-  /* eslint-enable */
