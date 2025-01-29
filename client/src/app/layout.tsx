b22321f97142b9c7d53ab190a552d05da8a53cb1
MFaad1 <faadsardar123@gmail.com>
2025-01-29 10:18:44 +0500

    fixed images upload issue

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${Helvetica.variable} ${Helveticalight.variable} ${belgium.variable} ${jadyn.variable}`}
+      className={` ${Helveticalight.variable} ${Helvetica.variable}  ${belgium.variable} ${jadyn.variable}`}
     >
       <body>
         <Providers>
